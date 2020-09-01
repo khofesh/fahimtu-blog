a8f3d29ca9dadb22ef8cedb02a093deef38fd90c
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-09-01 22:25:23 +0700

    change layout

--- src/components/BlogRoll.tsx
+++ src/components/BlogRoll.tsx
@@ -17,10 +17,10 @@ class BlogRoll extends React.Component<BlogRollProps> {
     const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <div className="columns is-multiline">
+      <div>
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
+            <div className="is-parent column is-12" key={post.id}>
               <article
                 className={`blog-list-item tile is-child box notification ${
                   post.frontmatter.featuredpost ? "is-featured" : ""
